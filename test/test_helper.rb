ac65a6ab833fcbb4c1fb6aefbb56ed3ef5789d3f
Stanislav (Stas) Katkov <git@skatkov.com>
2026-04-17 17:40:31 +0200

    fixing testsuit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,12 @@
 require_relative "test_app"
 require "rails/test_help"
+require "mutant/minitest/coverage"
 
-if ENV['SIMPLECOV']
-  require 'simplecov'
+if ENV["SIMPLECOV"]
+  require "simplecov"
 
   SimpleCov.start do
-    add_filter '/spec/'
+    add_filter "/spec/"
     minimum_coverage 100
     minimum_coverage_by_file 100
   end
